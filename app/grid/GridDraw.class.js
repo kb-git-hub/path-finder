eda4db439ecef19a8a84b932b87a8299aebf10d0
KB <kj.br@protonmail.com>
2022-11-12 23:17:14 -0600

    Update SVG

--- app/grid/GridDraw.class.js
+++ app/grid/GridDraw.class.js
@@ -16,9 +16,14 @@ class GridDraw {
         return gridCells.find((cell) => cell.isInCell);
     }
 
+    get pathElement() {
+        return document.querySelector('path');
+    }
+
     draw() {
         const {
             outCell,
+            pathElement,
             inCell,
             grid,
             grid: { svgElement },
@@ -26,6 +31,34 @@ class GridDraw {
         const gridPathFinder = new GridPathFinder({ grid, outCell, inCell });
 
         this.helperPath = gridPathFinder.generateHelperPath();
+
+        pathElement.setAttribute('d', this.buildPathD());
+    }
+
+    buildPathD() {
+        const {
+            outCell,
+            inCell,
+            grid: {
+                settings: { cellSize, borderSize },
+            },
+        } = this;
+
+        const [rowOut, colOut] = outCell.position.split('-').map((item) => +item);
+        const [rowIn, colIn] = inCell.position.split('-').map((item) => +item);
+
+        function generateM(startPos) {
+            return startPos * cellSize - cellSize * 0.5 + startPos * borderSize * 2;
+        }
+
+        const m1 = generateM(colOut);
+        const m2 = generateM(rowOut);
+
+        const pathD = `M${m1} ${m2}`;
+        const distance = cellSize + borderSize * 2;
+        console.log(pathD, distance);
+
+        return pathD;
     }
 }
 
